259d6b63fa7732649dc3e2353a3d3590837064d6
syedtahaahmed <51881361+syedtahaahmed@users.noreply.github.com>
2021-09-28 00:58:45 +0530

    Delete zoho-leads directory

--- zoho-leads/abc.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['submit'])){
-    $fname=$_POST['first'];
-    echo "First name:" .$fname."<br/>";
-    $lname=$_POST['last'];
-    echo "Last name:" .$lname."<br/>";
-    //insert_leads();
-}
-?>
